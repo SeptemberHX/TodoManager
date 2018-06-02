e52736e6d7336303518a9e6f5c818b5f6267dbda
SeptemberHX <tianjianshan@outlook.com>
2018-06-02 16:54:27 +0800

    Init config module

---
+++ config/TodoConfig.cpp
@@ -0,0 +1,5 @@
+//
+// Created by septemberhx on 6/2/18.
+//
+
+#include "TodoConfig.h"
---
+++ config/TodoConfig.h
@@ -0,0 +1,14 @@
+//
+// Created by septemberhx on 6/2/18.
+//
+
+#ifndef TODOMANAGER_TODOCONFIG_H
+#define TODOMANAGER_TODOCONFIG_H
+
+
+class TodoConfig {
+
+};
+
+
+#endif //TODOMANAGER_TODOCONFIG_H
