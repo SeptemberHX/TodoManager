bf2095e7246eba147e988cd89e03acc8da424f6b
SeptemberHX <tianjianshan@outlook.com>
2018-07-24 17:54:18 +0800

    Change: move core/gtest/TestCore.cpp to gtest directory. Now all gtest files are in the same place

--- gtest/TestCore.cpp
+++ gtest/TestCore.cpp
@@ -3,17 +3,12 @@
 //
 
 #include <gtest/gtest.h>
-#include "../ItemTag.h"
+#include "../core/ItemTag.h"
 
-TEST(core_test_case, core_test) {
+TEST(core, ItemTag) {
     todo::ItemTag tag1("1");
     todo::ItemTag tag2("1");
     todo::ItemTag tag3("2");
     EXPECT_EQ(tag1, tag2);
     EXPECT_NE(tag1, tag3);
-}
-
-int main(int argc, char *argv[]) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
